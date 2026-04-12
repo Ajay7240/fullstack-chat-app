8c1747267e1300cbe8498c7e82724409f70d0960
Ajay Kumar <kumawatajay7240@gmail.com>
2026-04-12 10:10:53 +0530

    production url changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,28 +2,27 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-);
+
+app.use(cors({
+    origin: process.env.NODE_ENV === "production"
+        ? "https://fullstack-chat-app-yrm1.onrender.com/"
+        : "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -31,12 +30,12 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    app.use((req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
     });
 }
 
 server.listen(PORT, () => {
-    console.log("server is running on PORT:" + PORT);
+    console.log(`Server is running on PORT: ${PORT}`);
     connectDB();
 });
